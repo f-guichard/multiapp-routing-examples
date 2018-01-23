0bcfc6fbe5f3ebe7d60cad9fae95f2020ea5cd51
Fabien Guichard <fabien.guichard@orange.com>
2018-01-23 02:20:10 +0100

    Corrections /proxy-page et /application

--- nodejs-app/apiserver.js
+++ nodejs-app/apiserver.js
@@ -1,5 +1,5 @@
+var httpproxy = require('http');
 var express = require('express');
-var redis = require('redis');
 
 var time = new Date().getTime();
 
@@ -19,6 +19,27 @@ vcapSenv = process.env.VCAP_SERVICES;
 
 var call = 0;
 
+//Register handler
+
+httpserver.get("/proxy-page", function(req, res) {
+   var proxyclient = httpproxy.request(
+     {
+       host: '254.254.254.254',
+       port: 9898,
+       user: '1',
+       password: '2',
+       method: 'GET',
+       path: 'http://perdu.com/'
+     }, function (response) {
+          response.on('data', function (body) {
+          console.log('Jai recu'+body.toString());
+        });
+     });
+   proxyclient.end();
+   res.send({"You requested this page : ": "url dune page"});
+   call += 1;
+});
+
 httpserver.get("/4", function(req, res) {
    var Client = req.socket['remoteAddress']+":"+req.socket['remotePort'];
    res.send({"NodeJS App4 health": "OK", "call": call});
@@ -37,8 +58,8 @@ httpserver.get("/services", function(req, res) {
 }); 
 
 httpserver.get("/application", function(req, res) {
-   var vcap_services = JSON.parse(process.env.VCAP_APPLICATION);
-   if (vcap_services != null) {
+   var vcap_application = JSON.parse(process.env.VCAP_APPLICATION);
+   if (vcap_application != null) {
      res.send({"vcap_application": vcap_application});
    }
    else {
